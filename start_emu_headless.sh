5dcb55fa4bd30a80a2b23f278135816010cc3550
Alan-R-Nunez <158207378+Alan-R-Nunez@users.noreply.github.com>
2024-11-08 10:40:56 -0400

    Update start_emu_headless.sh

--- start_emu_headless.sh
+++ start_emu_headless.sh
@@ -27,7 +27,7 @@ function check_hardware_acceleration() {
         fi
     fi
 
-    echo "$hw_accel_flag"
+    echo "Hardware accelaration: $hw_accel_flag"
 }
 
 hw_accel_flag=$(check_hardware_acceleration)
